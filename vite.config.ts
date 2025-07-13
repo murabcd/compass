45a390fd97c4978ceb89468706945c8d85686614
muradpm <abdulkadyr0v@yandex.ru>
2025-07-13 13:05:59 +0300

    add @vitejs/plugin-react to dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
 // vite.config.ts
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    tailwindcss(),
-    tsconfigPaths(),
-    tanstackStart({
-      tsr: {
-        routesDirectory: "src/routes",
-      },
-    }),
-  ],
+	server: {
+		port: 3000,
+	},
+	plugins: [
+		react(),
+		tailwindcss(),
+		tsconfigPaths(),
+		tanstackStart({
+			tsr: {
+				routesDirectory: "src/routes",
+			},
+			customViteReactPlugin: true,
+		}),
+	],
 });
